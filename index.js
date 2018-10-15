7c8f2f4f367bb1ef6b0022aab7ec55bbf155043e
Graeme <graemelearn@protonmail.com>
2018-10-15 15:49:57 -0700

    fix: update struct

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const files = fs.readdirSync(dir);
 
 files.forEach((fileName) => {
     if (fileName.endsWith(".json")) {
-        addressBook[fileName] = readJsonSync(`${dir}/${fileName}`);
+        exports[fileName] = readJsonSync(`${dir}/${fileName}`);
     }
 });
 
 const versions = Object.keys(addressBook).sort();
-addressBook["latest"] = addressBook[versions[0]];
 
 function readJsonSync(path) {
     return JSON.parse(fs.readFileSync(path, "utf8"));
 }
 
-exports.addressBook = addressBook;
+exports.latest = addressBook[versions[0]];
